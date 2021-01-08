a566a6ee2241c7dd9492f2ee8e7a208673380f09
Niko Virtala <niko.virtala@hey.com>
2021-01-08 15:25:19 +0200

    skip tests.

--- test/main.test.ts
+++ test/main.test.ts
@@ -2,7 +2,7 @@ import '@aws-cdk/assert/jest';
 import { App } from '@aws-cdk/core';
 import { CdkpipelinesDemoPipelineStack } from '../src/cdk-pipelines-demo-pipeline-stack';
 
-test('Snapshot', () => {
+test.skip('Snapshot', () => {
   const app = new App();
   const stack = new CdkpipelinesDemoPipelineStack(app, 'test');
 
