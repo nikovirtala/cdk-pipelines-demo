dd997d6fb64e82d8242850dba8551a110365eccd
Niko Virtala <niko.virtala@hey.com>
2021-02-16 14:21:29 +0200

    fix: add context to lambda handler.

--- lambda/main.ts
+++ lambda/main.ts
@@ -1,9 +1,9 @@
 const response_body = '<!DOCTYPE html><html><head><meta charset="UTF-8"><title>Honk!</title></head><body><pre>Honk!</pre></body></html>';
 
-exports.handler = (event, callback) =>
+exports.handler = (event, context, callback) =>
   callback(null, {
     statusCode: '200',
-    // body: JSON.stringify(event),
+    // body: JSON.stringify(event),event, context, callback
     body: response_body,
     headers: {
       'Content-Type': 'text/html',
